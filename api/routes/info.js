690ca2be0b2ee76059363af0ef75655ff6c178f1
Scott Mangiapane <scottmangiapane@gmail.com>
2020-10-12 00:36:28 -0700

    Removed websockets; added dynamic data endpoint

--- api/routes/info.js
+++ api/routes/info.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const info = require('../controllers/info');
 
+router.get('/dynamic', info.getDynamic);
+
 router.get('/static', info.getStatic);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 require('express-async-errors');
 
-const { spawn } = require('child_process');
 const express = require('express');
 const helmet = require('helmet');
 const logger = require('morgan');
-const os = require('os');
 const session = require('express-session');
-const si = require('systeminformation');
 const swaggerUI = require('swagger-ui-express');
-const WebSocket = require('ws');
 const YAML = require('yamljs');
 
 const RateLimiter = require('./api/utils/rate-limiter');
@@ -76,60 +72,4 @@ app.use((err, req, res, next) => {
     return res.status(500).json({ errors: [{ msg: 'Internal server error' }] });
 });
 
-const server = app.listen(port, () => console.log(`Server is running...`));
-
-// build dynamic data object
-
-const wss = new WebSocket.Server({ server });
-
-const data = {
-    cpu: {
-        cores: [],
-        loadavg: [],
-        nodes: [],
-        temp: null
-    },
-    memory: {
-        memTotal: null,
-        memUsed: null,
-        swapTotal: null,
-        swapUsed: null
-    },
-    time: {
-        uptime: null
-    }
-};
-
-setInterval(() => {
-    // CPU
-    data.cpu.cores = os.cpus().map(c => c.speed);
-    data.cpu.loadavg = os.loadavg();
-    spawn('sh', [
-        '-c',
-        'top -bn1 | '
-            + 'grep "Cpu(s)" | '
-            + 'sed "s/.*, *\\([0-9.]*\\)%* id.*/\\1/" | '
-            + 'awk "{print 100 - \\$1}"'
-    ]).stdout.on('data', o => {
-        while (data.cpu.nodes.length >= 60) {
-            data.cpu.nodes.shift();
-        }
-        data.cpu.nodes.push(parseFloat(o));
-    });
-    si.cpuTemperature(o => {
-        data.cpu.temp = Math.round(o.main);
-    });
-    // memory
-    si.mem(o => {
-        data.memory.memUsed = Math.round(o.active / 1000000);
-        data.memory.memTotal = Math.round(o.total / 1000000);
-        data.memory.swapUsed = Math.round(o.swapused / 1000000);
-        data.memory.swapTotal = Math.round(o.swaptotal / 1000000);
-    });
-    // uptime
-    data.time.uptime = si.time().uptime;
-    // send data to clients
-    wss.clients.forEach(client => {
-        client.send(JSON.stringify(data));
-    });
-}, 1000);
+app.listen(port, () => console.log(`Server is running...`));
